98ff0c558f8abbbab0dc80ed01855713784f607a
Cris <c.cheuquel01@ufromail.cl>
2024-04-11 09:43:47 -0400

    Cuadratica.calcularRaicesIguales caso reales iguales

--- src/main/java/Cuadratica.java
+++ src/main/java/Cuadratica.java
@@ -3,11 +3,17 @@ public class Cuadratica {
     public double discriminante(double A, double B, double C){
         return Math.pow(B, 2) - 4 * A * C;
     }
-    public double[] raicesReales(double A, double B, double C){
+    public double[] formulaCuadratica(double A, double B, double C){
         double[] raices = new double[2];
         double discriminante = discriminante(A, B, C);
         raices[0] = (-B + Math.sqrt(discriminante)) / (2 * A);
         raices[1] = (-B - Math.sqrt(discriminante)) / (2 * A);
+        return raices;
+    }
+    public double[] calcularRaicesIguales(double A, double B){
+        double[] raices = new double[2];
+        raices[0] = -B / (2 * A);
+        raices[1] = raices[0];
         return raices;
     }
     public void determinarTipoSolucion(double A, double B, double C){
