2dfa3bc9979dbd2315d71e7d0c48a9b8004c4763
Cris <c.cheuquel01@ufromail.cl>
2024-04-11 09:34:39 -0400

    Cuadratica.raicesReales() calcula las raices reales de la ec.

--- src/main/java/Cuadratica.java
+++ src/main/java/Cuadratica.java
@@ -3,4 +3,11 @@ public class Cuadratica {
     public double discriminante(double A, double B, double C){
         return Math.pow(B, 2) - 4 * A * C;
     }
+    public double[] raicesReales(double A, double B, double C){
+        double[] raices = new double[2];
+        double discriminante = discriminante(A, B, C);
+        raices[0] = (-B + Math.sqrt(discriminante)) / (2 * A);
+        raices[1] = (-B - Math.sqrt(discriminante)) / (2 * A);
+        return raices;
+    }
 }
