bc3c86670c5cb953b4a82847a8a7fcd5f68d03be
Arturo Rivas <a.rivas06@ufromail.cl>
2024-04-19 15:15:39 -0300

    Arregla control de excepciones a los scanners

--- src/test/java/sistemaLinealTest.java
+++ src/test/java/sistemaLinealTest.java
@@ -6,20 +6,20 @@ class sistemaLinealTest {
 
     @Test
     void det() {
-        assertEquals(sistemaLineal.det(new double[][]{{1,2},{3,4}}), -2);
-        assertEquals(sistemaLineal.det(new double[][]{{0,0},{0,0}}), 0);
+        assertEquals(SistemaLineal.det(new double[][]{{1,2},{3,4}}), -2);
+        assertEquals(SistemaLineal.det(new double[][]{{0,0},{0,0}}), 0);
     }
 
     @Test
     void calcularSoluciones() {
-        assertArrayEquals(sistemaLineal.calcularSoluciones(new double[][]{{1,-1},{1,1}},new double[]{1,1}), new double[]{1,0});
-        assertArrayEquals(sistemaLineal.calcularSoluciones(new double[][]{{1,-1},{3,1}},new double[]{2,2}), new double[]{1,-1});
+        assertArrayEquals(SistemaLineal.calcularSoluciones(new double[][]{{1,-1},{1,1}},new double[]{1,1}), new double[]{1,0});
+        assertArrayEquals(SistemaLineal.calcularSoluciones(new double[][]{{1,-1},{3,1}},new double[]{2,2}), new double[]{1,-1});
     }
 
     @Test
     void tieneInfinitas() {
-        assertTrue(sistemaLineal.tieneInfinitas(new double[][]{{1,1},{4,4}},new double[]{1,4}));
-        assertFalse(sistemaLineal.tieneInfinitas(new double[][]{{1,1},{4,4}},new double[]{1,5}));
+        assertTrue(SistemaLineal.tieneInfinitas(new double[][]{{1,1},{4,4}},new double[]{1,4}));
+        assertFalse(SistemaLineal.tieneInfinitas(new double[][]{{1,1},{4,4}},new double[]{1,5}));
     }
 
 }
