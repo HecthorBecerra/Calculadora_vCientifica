5d9e0efbbb76fef04f9d3f24293172ea65d849ab
Arturo Rivas <a.rivas06@ufromail.cl>
2024-04-19 14:55:58 -0300

    Pedir sistema de ecuacion lineal al usuario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,4 @@
 public class Main {
     public static void main(String[] args){
-        double[][] A = {
-            {1,1},
-            {1,2},
-        };
-        double[] B = {
-            1,
-            3,
-        };
-        sistemaLineal.copiarEnmain(A, B);
-    }
+        MenuPrincipal.ejecutarMenuPrincipal();
 }
